f5b77b6b3d707e51d41454cf7d223525819304fb
nana0995 <81056817+nana0995@users.noreply.github.com>
2021-04-27 14:48:26 -0600

    Update funciones.js

--- Lab-2/forrmulario/js/funciones.js
+++ Lab-2/forrmulario/js/funciones.js
@@ -2,7 +2,7 @@ function agregarElementos(){
 
 	elemento = document.getElementById("cuerpotabla");
 	var genero;
-	var genero = null;
+	
 	
 	var condicionActual; /* condicion actual*/
 
@@ -103,7 +103,7 @@ function validarEmail(email) { //Validamos para que solo se permita formato emai
 	var domain = email.substring(at + 1);
 	
 	// Consultar longitudes
-	if (local.length < 1 & local.length > 64 || domain.length < 4 || domain.length > 255)
+	if (local.length < 1 | local.length > 64 || domain.length < 4 || domain.length > 255)
 		return false;
 	
 	// Asegúrese de que el dominio local y el dominio no comience con un punto ni termine con un punto
